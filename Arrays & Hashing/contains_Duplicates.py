323a3f6b53b3f224f0ee459a6040b5e2df87a2e6
Alex Marek <alex@Alexs-MacBook-Pro.local>
2023-07-30 19:44:40 -0500

    valid_Anagram solved

--- Arrays & Hashing/contains_Duplicates.py
+++ Arrays & Hashing/contains_Duplicates.py
@@ -1,9 +1,12 @@
 #476ms(Beats 98.74%)/31.03mb (Beats 42%)
 #Converts the list into a set which can only have unique entries. After that just checks the length.
 #If the new list is shorter a duplicate must have been removed meaning True should be returned
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
+def containsDuplicate(nums: list[int]) -> bool:
         if len(set(nums)) < len(nums):
             return True
         else:
             return False
+        
+print(containsDuplicate(nums=[1,1,2,2,5,5]))
+print(containsDuplicate(nums=[1,2,3,4,5]))
+print(containsDuplicate(nums=[1,1,2,3,4,5]))
