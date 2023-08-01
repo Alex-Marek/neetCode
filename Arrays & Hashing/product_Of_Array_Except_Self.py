d1568461936d223a2d6bda709282916e49e9ab5d
Alex Marek <alex@Alexs-MacBook-Pro.local>
2023-08-01 18:42:16 -0500

    Updated to be the solution on the site for product_Of_Array_Except_Self

--- Arrays & Hashing/product_Of_Array_Except_Self.py
+++ Arrays & Hashing/product_Of_Array_Except_Self.py
@@ -1,18 +1,24 @@
-#Too slow to work for Leetcode
+#Solution from Leetcode video people
+# This problem can be divided into two steps for maximal efficiency
+# The first step is to get a prefix multiplier, this will be the result of multiplying every number before the num in question
+# The second step is to get the postfix multiplier, this is the result of multiplying every num after the num in question
+# Once you have both of those if you multiply them together then you have the answers
+
+# In this program we optimize for memory efficiency by storing the prefix in the results and then just replacing the values
+# when multiplying with the postfix later
+# To do this all that needed be done was reverse through the array and multiply
 def productExceptSelf(nums: list[int]) -> list[int]:
-    res = []
-    answers = {}
-    for idx, value in enumerate(nums):
-        curProd = 1
-        if value not in answers:
-            for kdx, m_Val in enumerate(nums):
-                if kdx != idx:
-                    curProd = curProd * m_Val
-        else:
-            res.append(answers[value])
-            break
-        res.append(curProd)
-    return res
+        res = [1] * len(nums)
+        
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        postfix = 1 
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
     
 
 
