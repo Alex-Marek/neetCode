eac75ed3d6914d8b5d237a37e982367b81efb806
Alex Marek <alex@Alexs-MacBook-Pro.local>
2023-08-07 16:52:18 -0500

    Learned how to solve longest_Repeating_Character_Replacement

--- Sliding Window/longest_Repeating_Character_Replacement.py
+++ Sliding Window/longest_Repeating_Character_Replacement.py
@@ -1,30 +1,23 @@
+# Solution from neetcode 
+# Code works by looping through the whole length of the word, every loop it is going to add to the dictionary a count of each
+# specific letter
+# After that it will check if the right pointer - the left pointer(+1) then subtracting the count of the most frequent character
+# if that is greater than k then it means the window isn't valid anymore and we need to move on so we move our left pointer
+# while of course making sure to decrement whatever the left most character is from our dict since there is now one less of it
 def characterReplacement(s: str, k: int) -> int:
-    # Find the letter that occurs most
-    maxNum = 0
-    maxLetter = "A"
-    for value in s:
-        if s.count(value) > maxNum:
-            maxLetter = value
-            maxNum = s.count(value)
-    print(maxLetter)
+    count = {}
+    
+    l = 0
+    maxf = 0
+    for r in range(len(s)):
+        count[s[r]] = 1 + count.get(s[r], 0)
+        maxf = max(maxf, count[s[r]])
 
-
-    # Get the position of the largest string of that letter
-    res = 0
-    longestL, longestR = 0,0
-    l,r = 0, 0
-    while r <= len(s):
-        curLength = len(set(s[l:r]))
-        if curLength >= len(s[l:r]) and s[l:l+1] == maxLetter:
-            if max(res, curLength) > res:
-                longestL = l
-                longestR = r - 1
-                res = curLength
-            r += 1
-        else:
+        if (r - l + 1) - maxf > k:
+            count[s[l]] -= 1
             l += 1
-    print(s[longestL:longestR])
-    print(longestL, longestR)
+
+    return (r - l + 1)
 
 print(characterReplacement("ABAB",2))
 print(characterReplacement("AABABBA",1))
