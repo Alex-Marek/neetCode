b643042e1e0a6f5a2f6225f31202089254015563
Alex Marek <acmarek@live.com>
2023-08-10 16:13:36 -0500

    Refactored valid_Anagram to be more readable using different method

--- Arrays & Hashing/valid_Anagram.py
+++ Arrays & Hashing/valid_Anagram.py
@@ -1,25 +1,19 @@
 # 54ms (Beats 91%)/ 16mb (Beats 65%)
-# Initially checks to see if the two words are the same length becauses if they're not then they can't be an anagram
-# Creates a dict and populates it with all the letters and their cooresponding amounts
-# Runs through the list of the potential anagram subtracting from the dict if it finds a letter they both have
-# Loops through the dict checking for any values greater than 0, if one is found then it means there was an excess letter
-# and therefore cannot be an anagram
+# Updated to be a more elegant easier to program solution with what I've learned
+# Basically the idea behind this one is that you can just populate both at the same time, and return if they're the same at
+# the end rather than adding one to a dict then subtracting the other and returning instantly if any character didn't match up
+# This solution may be marginally slower, but it is much easier to understand and write.
+# New speed = 62 ms (Beats 63%) / 16mb (Beats 63%)
 def isAnagram(s: str, t: str) -> bool:
-        if len(s) != len(t):
-              return False
-        o_Word = {}
-        for letter in s:
-            if letter in o_Word:
-                  o_Word[letter] += 1
-            else:
-                  o_Word[letter] = 1
-        for letter in t:
-            if letter in o_Word:
-                  o_Word[letter] -= 1
-        for value in o_Word:
-              if o_Word[value] > 0:
-                    return False
-        return True
+      if len(t) != len(s):
+            return False
+
+      s_Letters, t_Letters = {}, {}      
+      for idx in range(len(s)):
+            s_Letters[s[idx]] = 1 + s_Letters.get(s[idx], 0)
+            t_Letters[t[idx]] = 1 + t_Letters.get(t[idx], 0)
+
+      return s_Letters == t_Letters
                 
 
 print(isAnagram("anagram","nagaram"))
