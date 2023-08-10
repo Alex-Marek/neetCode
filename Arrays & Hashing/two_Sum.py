d3d2ed7f47df6e3c029fe608e2b7c391c18531c6
Alex Marek <acmarek@live.com>
2023-08-10 16:30:32 -0500

    Refactored two_Sum solution to be faster/more readable/more writable

--- Arrays & Hashing/two_Sum.py
+++ Arrays & Hashing/two_Sum.py
@@ -1,19 +1,18 @@
 # 431ms (Beats 42%) / 17mb (Beats 60%)
-# Subtracts every number from every other number in the last and checks if they equal the target value if they do return
+# Did this problem a long time ago (before I even started the github for this) so it def needed to be redone to be faster/better
+# This solution is cleaner easier to write/understand and faster 
+# The solution works by looping through all the values in the nums list given to us
+# Every time a new num is tried it calculates out the solution number for that number and adds it to the dict
+# Before doing that it checks whether the number is currently already in the dict, if it is then it will return that plus
+# the current index value since that ought to the solution
 def twoSum(nums: list[int], target: int) -> list[int]:
-        solNum = 0
-        sol = []
-        for x in range(len(nums)):
-            solNum = target - nums[x]
-            try:
-                posNum = nums.index(solNum, x+1)
-            except ValueError:
-                continue
-            else:
-                if len(sol) < 2:
-                    sol.append(posNum)
-                    sol.append(x)
-        return sol
+    solNumbers = {}
+    for idx, value in enumerate(nums):
+        solNum = target - value
+        if solNum in solNumbers:
+            return [solNumbers[solNum], idx]
+        solNumbers[value] = idx
+    return False
 
 print(twoSum([2,7,11,15],9))
 print(twoSum([3,2,4],6))
