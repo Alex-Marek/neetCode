658b96772dd8a08c6e4841d88b2201ec00f91630
Alex Marek <acmarek@live.com>
2023-08-09 19:12:21 -0500

    Different better solution that is still to slow for sliding_Window_Maximum

--- Sliding Window/sliding_Window_Maximum.py
+++ Sliding Window/sliding_Window_Maximum.py
@@ -1,40 +1,39 @@
-# Explanation: 
-# Window position                Max
-# ---------------               -----
-# [1  3  -1] -3  5  3  6  7       3
-#  1 [3  -1  -3] 5  3  6  7       3
-#  1  3 [-1  -3  5] 3  6  7       5
-#  1  3  -1 [-3  5  3] 6  7       5
-#  1  3  -1  -3 [5  3  6] 7       6
-#  1  3  -1  -3  5 [3  6  7]      7
-# returns [3,3,5,5,6,7]
-def calcNewMax(nums2 = []):
-    maxim = float('-inf')
-    for value in nums2:
-        maxim = max(maxim, value)
-    return maxim
-
 def maxSlidingWindow(nums: list[int], k: int) -> list[int]:
+    if k == 1:
+        return nums
     res = []
     r = k
-    curMax = float('-inf')
+
+    curWin = []
+    for value in nums[0:k]:
+        curWin.append(value)
+    curWin.sort(reverse=True)
+    res.append(curWin[0])
     
-    curMax = calcNewMax(nums[0:r])
-    res.append(curMax)
-
     while r < len(nums):
-        if nums[r] >= curMax:
-            curMax = nums[r]
-            res.append(curMax)
-        else:
-            if nums[r-k] == curMax:
-                curMax = calcNewMax(nums[r-k +1 : r+1])
-            res.append(curMax)
-        r += 1
-    return res
-            
+        if nums[r] >= curWin[0]: 
+            curWin.insert(0,nums[r])
+        elif nums[r] < curWin[0]:   
+            for idx in range(len(curWin)-1):
+                if nums[r] == curWin[idx]:
+                    curWin.insert(idx, nums[r])
+                    break
+                if nums[r] < curWin[idx] and nums[r] > curWin[idx+1]:
+                    curWin.insert(idx+1, nums[r])
+                    break    
+            else:
+                curWin.append(nums[r])
 
-print(maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3))
+        curWin.remove(nums[r-k])
+        res.append(curWin[0])
+        
+        r += 1
+    
+    return res
+
+print(maxSlidingWindow([9,10,9,-7,-4,-8,2,-6], 5), "Expecting: ", [10,10,9,2])
+print(maxSlidingWindow([1,3,1,2,0,5], 3), "Expecting: ", [3,3,2,5])
+print(maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3), "Expecting: ", [3,3,5,5,6,7])
 print(maxSlidingWindow([1], 1))
 print(maxSlidingWindow([1,-1], 1))
 print(maxSlidingWindow([5,3,4], 1))
